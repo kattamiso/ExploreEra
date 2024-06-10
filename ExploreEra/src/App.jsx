bd1b2bf0355dd53d9529e7ca66a71812ca3a19cf
Mari Mekhashishvili <marimexashishvili8@gmail.com>
2024-06-10 22:31:53 +0400

    ..

--- ExploreEra/src/App.jsx
+++ ExploreEra/src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from "./components/Authentication/SignIn";
 import SignUp from "../src/components/Authentication/SignUp";
 import BookingPages from './components/BookingPages/BookingPages';
+import Flights from './components/BookingPages/Flights/Flights';
+import Hotels from './components/BookingPages/Hotels/Hotels';
 
 function App() {
   return (
@@ -13,7 +15,9 @@ function App() {
           <Route path='/signin' element={<SignIn />} />
           <Route path='/signup' element={<SignUp />} />
           <Route path='/' element={<LandingPage />} />
-          <Route path='/bookingpages' element={<BookingPages/>}/>
+          <Route path='/bookingpages' element={<BookingPages />}/>
+          <Route path='/flights' component= {<Flights />}/>
+          <Route path='/hotels' component= {<Hotels />}/>
         </Routes>
       </Router>
     </>
--- ExploreEra/src/components/BookingPages/Menu.jsx
+++ ExploreEra/src/components/BookingPages/Menu.jsx
@@ -1,31 +1,38 @@
 import { useState } from 'react';
+import Flights from './Flights/Flights';
+import Hotels from './Hotels/Hotels';
 
-const Menu = () => {
+export default function Menu() {
   const List = [
-    { title: 'Flights', path: '/flights' },
-    { title: 'Stays', path: '/stays' },
+    { title: 'Flights', component: <Flights /> },
+    { title: 'Stays', component: <Hotels /> },
   ];
 
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const handleTabClick = (index) => {
+    setSelectedTab(index);
+  };
+
   return (
-    <div className="flex flex-row mt-32 max-w-[1200px] mx-auto">
-      {List.map((item, index) => (
-        <div
-          key={index}
-          value={selectedTab}
-          onChange={handleChange}
-          className="px-4 py-2 mr-4 text-gray-600 hover:text-orange-500"
-          activeClassName="text-orange-500"
-        >
-          {item.title}
-        </div>
-      ))}
+    <div>
+      <div className="flex flex-row mt-32 max-w-[1200px] mx-auto">
+        {List.map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleTabClick(index)}
+            className={`px-4 py-2 mr-4 cursor-pointer ${
+              selectedTab === index ? 'text-orange-500' : 'text-gray-600 hover:text-orange-500'
+            }`}
+          >
+            {item.title}
+          </div>
+        ))}
+      </div>
 
-
-      <div>
-
+      <div className="content mt-8">
+        {List[selectedTab].component}
       </div>
     </div>
   );
-};
-
-export default Menu;
+}
