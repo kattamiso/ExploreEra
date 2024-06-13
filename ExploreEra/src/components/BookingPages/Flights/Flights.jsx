cf9888a8df7ce518c15ea157495d445f49d552d7
Mari Mekhashishvili <marimexashishvili8@gmail.com>
2024-06-13 11:39:26 +0400

    ...

--- ExploreEra/src/components/BookingPages/Flights/Flights.jsx
+++ ExploreEra/src/components/BookingPages/Flights/Flights.jsx
@@ -1,7 +1,28 @@
 
 const Flights = () => {
   return (
-    <div>Flights</div>
+    <div className=" max-w-[1200px] mx-auto">
+
+
+      <div className=" flex justify-between">
+            <div className="w-[350px] flex justify-between py-3 px-5 h-[50px] border-2 border-orange-600 rounded-xl">
+                From Tbilisi 
+                <span>+</span>
+            </div>
+            <div className="w-[350px] flex justify-between py-3 px-5 h-[50px] border-2 border-orange-600 rounded-xl">
+                to Paris
+                <span>+</span>
+
+            </div>
+            <div className="w-[350px] py-3 px-5 h-[50px] border-2 border-orange-600 rounded-xl"> 
+              Calendar
+            </div> 
+          
+              <button>Search</button>
+
+      </div>
+
+    </div>
   )
 }
 
